1038411e273570e8f604ce83ff5bb13e2ab3674f
riverdev <yariv.rivercybe@gmail.com>
2025-03-08 07:18:47 +0200

    In this code session i installed firebase using ng add @angular/fire, the install process broke after i logged in to firebase and after i chose the project and app, i got a 'undefined path' error. I think it was trying to copy the API & project info into environment.ts. So wht i did was manualy create does variables, and i created one for each environment. I gave all the environments same value since i have only 1 firebase project (i could have created 3 firebase projects one for each env). I also edited the config.ts adding providers in appConfig. Also notice that a firebase.json was created but it is an empty object, it is relavent for firebase hosting which i am not using

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,37 @@
-import { ApplicationConfig, provideZoneChangeDetection, isDevMode } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { environment } from '../environments/environment';
 
+import {
+  ApplicationConfig,
+  provideZoneChangeDetection,
+  isDevMode,
+} from '@angular/core';
+
+// For router support
+import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
+
+// For PWA support
 import { provideServiceWorker } from '@angular/service-worker';
 
+// For Firebase support
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+
+// Setting appConfig object
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideServiceWorker('ngsw-worker.js', {
-            enabled: !isDevMode(),
-            registrationStrategy: 'registerWhenStable:30000'
-          })]
+  // Setting the providers for the application configuration
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+
+    // Provider for PWA support
+    provideServiceWorker('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      registrationStrategy: 'registerWhenStable:30000',
+    }),
+
+    // Providers for firebase
+    provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
+    provideFirestore(() => getFirestore()),
+  ],
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,15 @@
 export const environment = {
   production: true,
+
   configurationMessage: 'The configuration is: production',
   currentBuildVersion: import.meta.env['NG_APP_PRD_VERSION'],
+
+  firebaseConfig: {
+    apiKey: import.meta.env['NG_APP_PRD_FIREBASE_API_KEY'],
+    authDomain: import.meta.env['NG_APP_PRD_FIREBASE_AUTH_DOMAIN'],
+    projectId: import.meta.env['NG_APP_PRD_FIREBASE_PROJECT_ID'],
+    storageBucket: import.meta.env['NG_APP_PRD_FIREBASE_STORAGE_BUCKET'],
+    messagingSenderId: import.meta.env['NG_APP_PRD_FIREBASE_MESSAGING_SENDER_ID'],
+    appId: import.meta.env['NG_APP_PRD_FIREBASE_APP_ID'],
+  },
 };
