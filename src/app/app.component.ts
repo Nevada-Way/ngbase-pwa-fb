e72c5f6644a7259d8dbc9e0f577bf3dbe1c905d7
riverdev <yariv.rivercybe@gmail.com>
2025-03-14 17:19:02 +0200

    feat: Added update feature for the PWA, every time a new version is available then a modal pops out to enable user to choose yes/no accept the updated version .Also cleaned up the default app.comp...html so easier to see version changes, moved the style content into the component's *.scss file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,28 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SwUpdate } from '@angular/service-worker';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'ngbase-pwa-fb';
+
+  constructor(private swUpdate: SwUpdate) {}
+
+  ngOnInit() {
+    if (this.swUpdate.isEnabled) {
+      this.swUpdate.versionUpdates.subscribe((event) => {
+        if (
+          event.type === 'VERSION_READY' &&
+          confirm('Ne version available. Do you want to load it ?')
+        ) {
+          this.swUpdate.activateUpdate().then(() => window.location.reload());
+        }
+      });
+    }
+  }
 }
